2e44762b8dcb77f5f9d8934cc284456e5dd15dca
Niccolo Cecconi <niccolocecconi2000@gmail.com>
2017-06-26 14:55:03 +0100

    Initial commit

---
+++ prova1.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"html/template"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func loadPage(title string) *Page {
+	filename := title + ".txt"
+	body, _ := ioutil.ReadFile(filename)
+	return &Page{Title: title, Body: body}
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/"):]
+	p := loadPage(title)
+	p = &Page{Title: title}
+	t, _ := template.ParseFiles("template/home.html")
+	t.Execute(w, p)
+}
+
+func main() {
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeHandler)
+	http.ListenAndServe(":8080", r)
+}
